219c494c4b682a90ccb8a0fa0eb406fa64bcc25a
Dzmitry <manaxname@users.noreply.github.com>
2022-06-29 00:46:10 +0300

    Step 1

--- main.cs
+++ main.cs
@@ -1,1 +1,24 @@
+using System;
 
+namespace Sandbox
+{
+	class Program
+	{
+		public class Data
+		{
+			public string Name;
+			public string SurName;
+			public int Age;
+			public string Country;
+			public string Adress;
+			public string Zip;
+			public string City;
+			public double Money;
+		}
+
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+}
